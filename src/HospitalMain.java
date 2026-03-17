f81f58b0b518215e8b0e431ea4d24b1d29b17b74
roshnivekaria <roshni.vekaria@strathmore.edu>
2026-03-17 13:00:31 +0300

    Added dynamic input and exception handling

--- src/HospitalMain.java
+++ src/HospitalMain.java
@@ -1,14 +1,31 @@
-// MAIN CLASS
+import java.util.Scanner;
+
 public class HospitalMain {
 
     public static void main(String[] args) {
 
-        Patient patient = new Patient("Roshni");
-        Doctor doctor = new Doctor("Dr Manji");
-        Billing bill = new Billing();
+        Scanner sc = new Scanner(System.in);
 
-        patient.schedule();
-        doctor.work();
-        bill.showBill();
+        try {
+            System.out.print("Enter patient name: ");
+            String patientName = sc.nextLine();
+
+            System.out.print("Enter doctor name: ");
+            String doctorName = sc.nextLine();
+
+            Patient patient = new Patient(patientName);
+            Doctor doctor = new Doctor(doctorName);
+            Billing bill = new Billing();
+
+            patient.schedule();
+            doctor.work();
+            bill.showBill();
+
+        } catch (Exception e) {
+            System.out.println("Error: Invalid input!");
+        } finally {
+            sc.close();
+            System.out.println("Program ended safely.");
+        }
     }
 }
